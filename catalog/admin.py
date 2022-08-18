5d1a8fc325e08d19ab3c826344a6533ecd36e300
britsuno <nritsuno@aol.com>
2022-08-17 22:50:45 -0700

    edited admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -8,10 +8,11 @@ class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
     extra = 0
 
+class BooksInline(admin.TabularInline):
+    model = Book
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre', 'language')
-
     inlines = [BooksInstanceInline]
 
 @admin.register(BookInstance)
@@ -32,15 +33,15 @@ class AuthorInline(admin.TabularInline):
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]    
 
-    fields = ['first_name', 'last_name', ('date_of_birth')]    
-
-    exclude = ['date_of_death']
+   """ exclude = ['date_of_death']"""
    
-    inlines = [AuthorInline] 
+    inlines = [BooksInline] 
 
 # admin.site.register(Book)
 # admin.site.register(Author)
+admin.site.register(Book, BookAdmin)
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Genre)
 # admin.site.register(BookInstance)
